3e8213386ec1b316b0ff0f2e1c570abc464f2f47
raheenah <raheenah500@gmail.com>
2026-01-21 10:09:09 +0100

    took out app routes

--- src/router.tsx
+++ src/router.tsx
@@ -4,37 +4,37 @@ import { lazy } from "react";
 
 const LandingPage = lazy(() => import("./components/webPage/landing"));
 
-const SignUp = lazy(() => import("./components/onboarding/signup"));
+// const SignUp = lazy(() => import("./components/onboarding/signup"));
 
-const Dashboard = lazy(() => import("./components/dashboard/layout/layout"));
-const DashboardLanding = lazy(
-  () => import("./components/dashboard/landing/dashboard")
-);
-const EnvironmentalDashboard = lazy(
-  () => import("./components/dashboard/environmental/environmental-manu/environmental")
-);
+// const Dashboard = lazy(() => import("./components/dashboard/layout/layout"));
+// const DashboardLanding = lazy(
+//   () => import("./components/dashboard/landing/dashboard")
+// );
+// const EnvironmentalDashboard = lazy(
+//   () => import("./components/dashboard/environmental/environmental-manu/environmental")
+// );
 
 export const router = createBrowserRouter([
   {
     path: "/",
     element: <LandingPage />,
   },
-  {
-    path: "/signup",
-    element: <SignUp />,
-  },
-  {
-    path: "/dashboard",
-    element: <Dashboard />,
-    children: [
-      {
-        index: true,
-        element: <DashboardLanding />,
-      },
-      {
-        path: "environmental",
-        element: <EnvironmentalDashboard />,
-      },
-    ],
-  },
+  // {
+  //   path: "/signup",
+  //   element: <SignUp />,
+  // },
+  // {
+  //   path: "/dashboard",
+  //   element: <Dashboard />,
+  //   children: [
+  //     {
+  //       index: true,
+  //       element: <DashboardLanding />,
+  //     },
+  //     {
+  //       path: "environmental",
+  //       element: <EnvironmentalDashboard />,
+  //     },
+  //   ],
+  // },
 ]);
